eda72f022194342018be232d4238c603bcec4733
XxshiftxX <contact@dev-shift.me>
2022-05-23 20:25:09 +0900

    기존 페이지 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import Broadcast from './page/Broadcast/Broadcast';
-import Dashboard from './page/Dashboard/Dashboard';
-import Login from './page/Login/Login';
-
 import './App.css';
-import Register from './page/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Dashboard} exact />
-      <Route path="/login" component={Login} exact />
-      <Route path="/register" component={Register} exact />
-      <Route path="/broadcast" component={Broadcast} exact />
     </div>
   );
 }
