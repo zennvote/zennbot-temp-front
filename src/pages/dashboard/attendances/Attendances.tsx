b802c764458a09bb62e7181a824df7fce8121974
XxshiftxX <contact@dev-shift.me>
2022-07-08 18:29:44 +0900

    잘못 적용된 마크업 수정

--- src/pages/dashboard/attendances/Attendances.tsx
+++ src/pages/dashboard/attendances/Attendances.tsx
@@ -9,17 +9,13 @@ export const Attendances: FC = () => {
       <div className="table">
         <ul className="header row">
           <li className="twitchId">아이디</li>
-          <li className="username">닉네임</li>
-          <li className="ticketPiece">조각</li>
-          <li className="ticket">티켓</li>
-          <li className="etc">비고</li>
+          <li className="attendedAt">출석 시각</li>
+          <li className="tier">티어</li>
         </ul>
         <ul className="row">
           <li className="twitchId">producerzenn</li>
-          <li className="username">프로듀서_젠</li>
-          <li className="ticketPiece">103</li>
-          <li className="ticket">0</li>
-          <li className="etc">[★료A★][765-하루카,리츠코,치즈루][346-쇼코,토키코,히카루,세츠나,오토하,소라,와카바,미야코,칸나,토코.하스미,요리코,노아,아야카][315-카논,하야토,모후모후엔,F-LAGS,THE코가도,Altessimo][283-키리코,나츠하,코이토] 담당 프로듀서</li>
+          <li className="attendedAt">2022-07-07 15:44:58</li>
+          <li className="tier">3</li>
         </ul>
       </div>
     </div>
