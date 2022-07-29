c86edf6cb1b015c37ca3800df9934a8cb57b38a3
XxshiftxX <contact@dev-shift.me>
2022-07-29 14:27:37 +0900

    변경된 출석 UI 반영

--- src/pages/dashboard/attendances/Attendances.tsx
+++ src/pages/dashboard/attendances/Attendances.tsx
@@ -1,30 +1,43 @@
-import { FC } from 'react';
-import { useAttendances } from 'src/hooks/useAttendances';
+import { FC, useState } from 'react';
+import { ChevronsLeft, ChevronsRight } from 'react-feather';
 
 import './Attendances.scoped.scss';
+import { AttendanceList } from './components/AttendanceList';
+
+const getBaseDay = () => {
+  const now = new Date();
+  return new Date(now.setDate(now.getDate() - now.getDay()));
+} 
 
 export const Attendances: FC = () => {
-  const { attendances } = useAttendances();
+  const [baseDay, setBaseDay] = useState(getBaseDay());
+  const dates = new Array(7).fill(null).map((n, index) => new Date(new Date(baseDay).setDate(baseDay.getDate() + index)));
+
+  const moveWeek = (isNext: boolean) => {
+    const acc = isNext ? 7 : -7;
+    setBaseDay(new Date(new Date(baseDay).setDate(baseDay.getDate() + acc)));
+  }
+
+  const resetWeek = () => setBaseDay(getBaseDay());
 
   return (
     <div className="root">
       <h1>출석체크 관리</h1>
-      <div className="table">
-        <ul className="header row">
-          <li className="twitchId">아이디</li>
-          <li className="attendedAt">출석 시각</li>
-          <li className="tier">티어</li>
-        </ul>
+      <div className="topbar">
+        <h2>{ baseDay.getFullYear() }년 { baseDay.getMonth() + 1 }월</h2>
+        <div className="navigator">
+          <ChevronsLeft size={28} onClick={() => moveWeek(false)} />
+          <button onClick={resetWeek}>오늘</button>
+          <ChevronsRight size={28} onClick={() => moveWeek(true)} />
+        </div>
+      </div>
+      <ul className="dates">
         {
-          attendances?.map((attendance) => (
-            <ul className="row" id={`${attendance.twitchId}-${attendance.attendedAt.getTime()}`}>
-              <li className="twitchId">{ attendance.twitchId }</li>
-              <li className="attendedAt">{ attendance.attendedAtString }</li>
-              <li className="tier">{ attendance.tier }</li>
-            </ul>
+          dates.map((date) => (
+            <AttendanceList broadcastDate={date} />
           ))
         }
-      </div>
+      </ul>
     </div>
   );
 };
---
+++ src/pages/dashboard/attendances/components/AttendanceList.tsx
@@ -0,0 +1,63 @@
+import { FC, useEffect, useState } from 'react';
+import { ChevronDown } from 'react-feather';
+import { useAttendancesForBroadcast } from 'src/hooks/useAttendancesForBroadcast';
+
+import './AttendanceList.scoped.scss';
+
+const parseDate = (date: Date) => {
+  const year = date.getFullYear();
+  let month: string | number = date.getMonth() + 1;
+  month = month >= 10 ? month : '0' + month;
+  let day: string | number = date.getDate();
+  day = day >= 10 ? day : '0' + day;
+  console.log(date, year, month, day);
+
+  return `${year}-${month}-${day}`;
+};
+
+const dayList = ['일', '월', '화', '수', '목', '금', '토'];
+
+type AttendanceListProps = { broadcastDate: Date }
+export const AttendanceList: FC<AttendanceListProps> = ({ broadcastDate }) => {
+  const { attendances } = useAttendancesForBroadcast(parseDate(broadcastDate));
+  const [isCollapsed, setCollapsed] = useState(false);
+
+  useEffect(() => setCollapsed(false), [broadcastDate])
+
+  const handleClickHeader = () => {
+    setCollapsed(!isCollapsed);
+  }
+
+  return (
+    <li className="root">
+      <div className="header" onClick={handleClickHeader}>
+        <h3 className="title">{ parseDate(broadcastDate) } ({ dayList[broadcastDate.getDay()] })</h3>
+        <ChevronDown />
+      </div>
+      {
+        isCollapsed && (
+          attendances?.length  ? 
+          <div className="table">
+            <ul className="header row">
+              <li className="twitchId">아이디</li>
+              <li className="attendedAt">출석 시각</li>
+              <li className="tier">티어</li>
+            </ul>
+            {
+              attendances?.map((attendance) => (
+                <ul className="row" id={`${attendance.twitchId}-${attendance.attendedAt.getTime()}`}>
+                  <li className="twitchId">{ attendance.twitchId }</li>
+                  <li className="attendedAt">{ attendance.attendedAtString }</li>
+                  <li className="tier">{ attendance.tier }</li>
+                </ul>
+              ))
+            }
+          </div> :
+          <div className="empty">
+            출석이 없습니다!
+          </div>
+        )
+      }
+    </li>
+  );
+};
